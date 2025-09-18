d4522af95e356ee6e1dc11958cc81f8939b8d2cb
Corvin Vale <corvin@valeant.space>
2025-09-18 02:06:44 -0400

    infra: observability tool storage

---
+++ infra/terraform/cloudflare/environments/hzl/loki.tf
@@ -0,0 +1,5 @@
+resource "cloudflare_r2_bucket" "hzl-loki-tsdb-weu" {
+  account_id = var.cloudflare_account_id
+  name       = "hzl-loki-tsdb-weu"
+  location   = "weur"
+}
--- infra/terraform/cloudflare/environments/hzl/variables.tf
+++ infra/terraform/cloudflare/environments/hzl/variables.tf
@@ -1,7 +1,7 @@
-# variable "cloudflare_account_id" {
-#   description = "The Cloudflare account ID to use for resources"
-#   type        = string
-# }
+variable "cloudflare_account_id" {
+  description = "The Cloudflare account ID to use for resources"
+  type        = string
+}
 
 variable "cloudflare_api_token" {
   description = "API token for Cloudflare"
